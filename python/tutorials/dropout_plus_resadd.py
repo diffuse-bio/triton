cc77b35cb1261715e20f79178ff6499ae2ba0676
Namrata Anand <namrata@diffuse.bio>
2023-11-04 13:30:05 -0700

    dropout: bwd pass

--- python/tutorials/dropout_plus_resadd.py
+++ python/tutorials/dropout_plus_resadd.py
@@ -8,6 +8,7 @@ import torch
 
 import triton
 import triton.language as tl
+import torch.nn.functional as F
 
 try:
     # This is https://github.com/NVIDIA/apex, NOT the apex on PyPi, so it
@@ -24,6 +25,7 @@ def _drcln_fwd_fused(
     Y,  # pointer to the output
     Z, # pointer to the input to dropout
     Z_out, # pointer to output after dropout
+    MASK_out, # pointer to output mask after dropout
     p, # dropout prob
     seed, # dropout seed
     # W,  # pointer to the weights
@@ -42,6 +44,7 @@ def _drcln_fwd_fused(
     Y += row * stride
     X += row * stride
     Z_out += row * stride
+    MASK_out += row * stride
 
 
     # do dropout on Z
@@ -54,7 +57,7 @@ def _drcln_fwd_fused(
         x_keep = random > p
         # _mean += a
         output = tl.where(x_keep, z / (1 - p), 0.0)
-
+        tl.store(MASK_out + cols, x_keep, mask=mask)
         tl.store(Z_out + cols, output, mask=mask)
 
 
@@ -108,19 +111,25 @@ def _drcln_fwd_fused(
 
 @triton.jit
 def _drcln_bwd_dx_fused(
-    DX,  # pointer to the input gradient
-    DY,  # pointer to the output gradient
-    DW,  # pointer to the partial sum of weights gradient
-    DB,  # pointer to the partial sum of biases gradient
-    X,   # pointer to the input
-    W,   # pointer to the weights
-    B,   # pointer to the biases
-    Mean,   # pointer to the mean
-    Rstd,   # pointer to the 1/std
-    Lock,  # pointer to the lock
+    DY, # pointer to output gradient
+    DZ, # pointer to input gradient
+    # Z, # pointer to input
+    DROPOUT_MASK, # pointer to dropout mask
+    p, # dropout rate
+
+    # DX,  # pointer to the input gradient
+    # DY,  # pointer to the output gradient
+    # DW,  # pointer to the partial sum of weights gradient
+    # DB,  # pointer to the partial sum of biases gradient
+    # X,   # pointer to the input
+    # W,   # pointer to the weights
+    # B,   # pointer to the biases
+    # Mean,   # pointer to the mean
+    # Rstd,   # pointer to the 1/std
+    # Lock,  # pointer to the lock
     stride,  # how much to increase the pointer when moving by 1 row
     N,  # number of columns in X
-    eps,  # epsilon to avoid division by zero
+    # eps,  # epsilon to avoid division by zero
     GROUP_SIZE_M: tl.constexpr,
     BLOCK_SIZE_N: tl.constexpr
 ):
@@ -128,62 +137,98 @@ def _drcln_bwd_dx_fused(
     row = tl.program_id(0)
     cols = tl.arange(0, BLOCK_SIZE_N)
     mask = cols < N
-    X += row * stride
+    # Z += row * stride
+    DROPOUT_MASK += row * stride
     DY += row * stride
-    DX += row * stride
-    # Also map the program id to the elements of W it should compute (per-row)
-    W += row * stride
-    # 
-    if False:
-        # Offset locks and weights/biases gradient pointer for parallel reduction
-        lock_id = row % GROUP_SIZE_M
-        Lock += lock_id
-        Count = Lock + GROUP_SIZE_M
-        DW = DW + lock_id * N + cols
-        DB = DB + lock_id * N + cols
-    # Load data to SRAM
-    x = tl.load(X + cols, mask=mask, other=0).to(tl.float32)
-    dy = tl.load(DY + cols, mask=mask, other=0).to(tl.float32)
-    w = tl.load(W + cols, mask=mask).to(tl.float32)
-    # dw = tl.load(DW + cols, mask=mask).to(tl.float32)
-    # db = tl.load(DB + cols, mask=mask).to(tl.float32)
+    DZ += row * stride
 
-    mean = tl.load(Mean + row)
-    rstd = tl.load(Rstd + row)
-    # Compute dx
-    xhat = (x - mean) * rstd
-    wdy = w * dy
-    xhat = tl.where(mask, xhat, 0.)
-    wdy = tl.where(mask, wdy, 0.)
-    c1 = tl.sum(xhat * wdy, axis=0) / N
-    c2 = tl.sum(wdy, axis=0) / N
-    dx = (wdy - (xhat * c1 + c2)) * rstd
-    # Write dx
-    tl.store(DX + cols, dx, mask=mask)
-    # Accumulate partial sums for dw/db
-    partial_dw = (dy * xhat).to(w.dtype)
-    partial_db = (dy).to(w.dtype)
+
+    # dL/dz = DL/dy dy/dz
+    # z_out = mask * z / (1 - p)
+    # gradient is  dz = dy * mask / (1 - p)
+    # for off in range(0, N, BLOCK_SIZE):
+    cols = tl.arange(0, BLOCK_SIZE_N)
+    mask = cols < N
+    dy = tl.load(DY + cols, mask=mask, other=0).to(tl.float32)
+    dropout_mask = tl.load(DROPOUT_MASK + cols, mask=mask, other=0.).to(tl.float32)
+    dz = dy * dropout_mask / (1 - p)
+    tl.store(DZ + cols, dz, mask=mask)
+
+
+
+    # # do dropout on Z
+    # for off in range(0, N, BLOCK_SIZE):
+    #     all_elem = row * stride + (off + tl.arange(0, BLOCK_SIZE))
+    #     cols = off + tl.arange(0, BLOCK_SIZE)
+    #     mask = cols < N
+    #     z = tl.load(Z + cols, mask=mask, other=0.).to(tl.float32)
+    #     random = tl.rand(seed, all_elem)
+    #     x_keep = random > p
+    #     # _mean += a
+    #     output = tl.where(x_keep, z / (1 - p), 0.0)
+    #     tl.store(MASK_out + cols, x_keep, mask=mask)
+    #     tl.store(Z_out + cols, output, mask=mask)
+
+
+
+    # X += row * stride
+    # DY += row * stride
+    # DZ += row * stride
+
+
+    # # Also map the program id to the elements of W it should compute (per-row)
+    # W += row * stride
+    # # 
+    # if False:
+    #     # Offset locks and weights/biases gradient pointer for parallel reduction
+    #     lock_id = row % GROUP_SIZE_M
+    #     Lock += lock_id
+    #     Count = Lock + GROUP_SIZE_M
+    #     DW = DW + lock_id * N + cols
+    #     DB = DB + lock_id * N + cols
+    # # Load data to SRAM
+    # x = tl.load(X + cols, mask=mask, other=0).to(tl.float32)
+    # dy = tl.load(DY + cols, mask=mask, other=0).to(tl.float32)
+    # w = tl.load(W + cols, mask=mask).to(tl.float32)
+    # # dw = tl.load(DW + cols, mask=mask).to(tl.float32)
+    # # db = tl.load(DB + cols, mask=mask).to(tl.float32)
+
+    # mean = tl.load(Mean + row)
+    # rstd = tl.load(Rstd + row)
+    # # Compute dx
+    # xhat = (x - mean) * rstd
+    # wdy = w * dy
+    # xhat = tl.where(mask, xhat, 0.)
+    # wdy = tl.where(mask, wdy, 0.)
+    # c1 = tl.sum(xhat * wdy, axis=0) / N
+    # c2 = tl.sum(wdy, axis=0) / N
+    # dx = (wdy - (xhat * c1 + c2)) * rstd
+    # # Write dx
+    # tl.store(DX + cols, dx, mask=mask)
+    # # Accumulate partial sums for dw/db
+    # partial_dw = (dy * xhat).to(w.dtype)
+    # partial_db = (dy).to(w.dtype)
     
-    # partial_dw = dw + (dy * xhat).to(w.dtype)
-    # partial_db = db + (dy).to(w.dtype)
-    if False:
-        while tl.atomic_cas(Lock, 0, 1) == 1:
-            pass
-        count = tl.load(Count)
-        # First store doesn't accumulate
-        if count == 0:
-            tl.atomic_xchg(Count, 1)
-        else:
-            partial_dw += tl.load(DW, mask=mask)
-            partial_db += tl.load(DB, mask=mask)
-    # no reduction needed (no sum over batch to get grads DW and DB)
-    DW += row * stride
-    DB += row * stride
-    tl.store(DW + cols, partial_dw, mask=mask)
-    tl.store(DB + cols, partial_db, mask=mask)
-    if False:
-        # Release the lock
-        tl.atomic_xchg(Lock, 0)
+    # # partial_dw = dw + (dy * xhat).to(w.dtype)
+    # # partial_db = db + (dy).to(w.dtype)
+    # if False:
+    #     while tl.atomic_cas(Lock, 0, 1) == 1:
+    #         pass
+    #     count = tl.load(Count)
+    #     # First store doesn't accumulate
+    #     if count == 0:
+    #         tl.atomic_xchg(Count, 1)
+    #     else:
+    #         partial_dw += tl.load(DW, mask=mask)
+    #         partial_db += tl.load(DB, mask=mask)
+    # # no reduction needed (no sum over batch to get grads DW and DB)
+    # DW += row * stride
+    # DB += row * stride
+    # tl.store(DW + cols, partial_dw, mask=mask)
+    # tl.store(DB + cols, partial_db, mask=mask)
+    # if False:
+    #     # Release the lock
+    #     tl.atomic_xchg(Lock, 0)
 
 
 # %%
@@ -198,13 +243,14 @@ def _drcln_bwd_dx_fused(
 class DropoutResAddCLN(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, z, x, normalized_shape, p=0.5, seed=42, eps=1e-5):
+    def forward(ctx, z, x, p=0.5, seed=42, eps=1e-5):
         # z --> input ahead of dropout
         # x --> original input (residual add)
 
         # allocate output
         y = torch.empty_like(x)
         z_out = torch.empty_like(z)
+        mask_out = torch.empty_like(z)
         # reshape input data into 2D tensor
         x_arg = x.reshape(-1, x.shape[-1])
         M, N = x_arg.shape
@@ -222,61 +268,81 @@ class DropoutResAddCLN(torch.autograd.Function):
 
 
 
-        _drcln_fwd_fused[(M,)](x_arg, y, z, z_out, p, seed, #mean, rstd,
+        _drcln_fwd_fused[(M,)](x_arg, y, z, z_out, mask_out, p, seed, #mean, rstd,
                                     x_arg.stride(0), N, #eps,
                                     BLOCK_SIZE=BLOCK_SIZE, num_warps=num_warps, num_ctas=1)
-        # ctx.save_for_backward(x, weight, bias, mean, rstd)
-        # ctx.BLOCK_SIZE = BLOCK_SIZE
-        # ctx.num_warps = num_warps
-        # ctx.eps = eps
-        return z_out
+        ctx.save_for_backward(mask_out, ) #x, weight, bias, mean, rstd)
+        ctx.BLOCK_SIZE = BLOCK_SIZE
+        ctx.num_warps = num_warps
+        ctx.eps = eps
+        # ctx.mask_out = mask_out
+        ctx.p = p
+        return z_out #, mask_out
 
     @staticmethod
     def backward(ctx, dy):
-        x, w, b, m, v = ctx.saved_tensors
+        mask, = ctx.saved_tensors
         # heuristics for amount of parallel reduction stream for DW/DB
-        N = w.shape[0]
+        M, N = dy.shape #[1]
         GROUP_SIZE_M = 64
         if N <= 8192: GROUP_SIZE_M = 96
         if N <= 4096: GROUP_SIZE_M = 128
         if N <= 1024: GROUP_SIZE_M = 256
 
-        # dw and db are now (M, N) instead of N
-        dw = torch.empty((w.shape[0], w.shape[1]), dtype=w.dtype, device=w.device)
-        db = torch.empty((w.shape[0], w.shape[1]), dtype=w.dtype, device=w.device)
-        if False:
-            # allocate output
-            locks = torch.zeros(2 * GROUP_SIZE_M, dtype=torch.int32, device='cuda')
-            _dw = torch.empty((GROUP_SIZE_M, w.shape[0]), dtype=x.dtype, device=w.device)
-            _db = torch.empty((GROUP_SIZE_M, w.shape[0]), dtype=x.dtype, device=w.device)
-            dw = torch.empty((w.shape[0],), dtype=w.dtype, device=w.device)
-            db = torch.empty((w.shape[0],), dtype=w.dtype, device=w.device)
-        dx = torch.empty_like(dy)
-        # enqueue kernel using forward pass heuristics
-        # also compute partial sums for DW and DB
-        x_arg = x.reshape(-1, x.shape[-1])
-        M, N = x_arg.shape
-        _drcln_bwd_dx_fused[(M,)](dx, dy, dw, db, x, w, b, m, v, None,
-                                       x_arg.stride(0), N, ctx.eps,
-                                       BLOCK_SIZE_N=ctx.BLOCK_SIZE,
-                                       GROUP_SIZE_M=GROUP_SIZE_M,
-                                       num_warps=ctx.num_warps)
-        # no need to accumulate partial sums 
-        if False:
+        # x_arg = x.reshape(-1, x.shape[-1])
+        # M, N = x_arg.shape
+        dz = torch.empty_like(dy)
+        _drcln_bwd_dx_fused[(M,)](dy, dz,mask, ctx.p,
+                                    dy.stride(0), N, #ctx.eps,
+                                    BLOCK_SIZE_N=ctx.BLOCK_SIZE,
+                                    GROUP_SIZE_M=GROUP_SIZE_M,
+                                    num_warps=ctx.num_warps)
+        
+        return dz, None
+        # # dw and db are now (M, N) instead of N
+        # dw = torch.empty((w.shape[0], w.shape[1]), dtype=w.dtype, device=w.device)
+        # db = torch.empty((w.shape[0], w.shape[1]), dtype=w.dtype, device=w.device)
+        # if False:
+        #     # allocate output
+        #     locks = torch.zeros(2 * GROUP_SIZE_M, dtype=torch.int32, device='cuda')
+        #     _dw = torch.empty((GROUP_SIZE_M, w.shape[0]), dtype=x.dtype, device=w.device)
+        #     _db = torch.empty((GROUP_SIZE_M, w.shape[0]), dtype=x.dtype, device=w.device)
+        #     dw = torch.empty((w.shape[0],), dtype=w.dtype, device=w.device)
+        #     db = torch.empty((w.shape[0],), dtype=w.dtype, device=w.device)
+        # dx = torch.empty_like(dy)
+        # # enqueue kernel using forward pass heuristics
+        # # also compute partial sums for DW and DB
+        # x_arg = x.reshape(-1, x.shape[-1])
+        # M, N = x_arg.shape
+        # _drcln_bwd_dx_fused[(M,)](dx, dy, dw, db, x, w, b, m, v, None,
+        #                                x_arg.stride(0), N, ctx.eps,
+        #                                BLOCK_SIZE_N=ctx.BLOCK_SIZE,
+        #                                GROUP_SIZE_M=GROUP_SIZE_M,
+        #                                num_warps=ctx.num_warps)
+        # # no need to accumulate partial sums 
+        # if False:
             
-            grid = lambda meta: [triton.cdiv(N, meta['BLOCK_SIZE_N'])]
-            # accumulate partial sums in separate kernel
-            _drcln_bwd_dwdb[grid](_dw, _db, dw, db, GROUP_SIZE_M, N,
-                                    BLOCK_SIZE_M=32,
-                                    BLOCK_SIZE_N=128, num_ctas=1)
-        return dx, None, dw, db, None
+        #     grid = lambda meta: [triton.cdiv(N, meta['BLOCK_SIZE_N'])]
+        #     # accumulate partial sums in separate kernel
+        #     _drcln_bwd_dwdb[grid](_dw, _db, dw, db, GROUP_SIZE_M, N,
+        #                             BLOCK_SIZE_M=32,
+        #                             BLOCK_SIZE_N=128, num_ctas=1)
+
+        return dz #, None
+        # return dx, None, dw, db, None
 
 
 dracln = DropoutResAddCLN.apply
 
-def vanilla_conditional_drcln(x, weight, bias, eps=1e-5):
+def vanilla_conditional_drcln(x): #, weight, bias, eps=1e-5):
     # vanilla CLN --> different scale (weight) and shift (bias) params per element in batch
     M, N = x.size()
+    p = 0.5
+    x_out = F.dropout(x, p=0.5)
+    return x_out
+    x_keep = (torch.rand(size=(10,)) > p).to(torch.int32).cuda()
+
+
     assert weight.size() == x.size()
     assert bias.size() == x.size()
     mean = torch.mean(x, -1)[..., None]
@@ -298,25 +364,35 @@ def test_drcln(M, N, dtype, eps=1e-5, device='cuda'):
     weight = torch.rand(x_shape, dtype=dtype, device='cuda', requires_grad=True)
     bias = torch.rand(x_shape, dtype=dtype, device='cuda', requires_grad=True)
     x = -2.3 + 0.5 * torch.randn(x_shape, dtype=dtype, device='cuda')
+    z = -2.3 + 0.5 * torch.randn(x_shape, dtype=dtype, device='cuda')
     dy = .1 * torch.randn_like(x)
     x.requires_grad_(True)
+    z.requires_grad_(True)
     # forward pass
 
-    z_out_tri = dracln(x, x, w_shape) #, p=0.5, seed=123)
+    z_out_tri = dracln(z, x) #, w_shape) #, p=0.5, seed=123)
     print(z_out_tri[0], z_out_tri[1], z_out_tri[-1])
+    # print(mask_out_tri[0], mask_out_tri[1], mask_out_tri[-1])
     # y_tri = layer_norm(x, w_shape, weight, bias, eps)
-    # y_ref = vanilla_conditional_drcln(x, weight, bias, eps).to(dtype) #torch.nn.functional.layer_norm(x, w_shape, weight, bias, eps).to(dtype)
+    z_out_ref = vanilla_conditional_drcln(x) #, weight, bias, eps).to(dtype) #torch.nn.functional.layer_norm(x, w_shape, weight, bias, eps).to(dtype)
     # assert torch.allclose(y_tri, y_ref, atol=1e-2, rtol=0)
+    print(z_out_ref[0], z_out_ref[-1])
+
 
 
     # # # backward pass (triton)
-    # y_tri.backward(dy, retain_graph=True)
-    # dx_tri, dw_tri, db_tri = [_.grad.clone() for _ in [x, weight, bias]]
-    # x.grad, weight.grad, bias.grad = None, None, None
-    # # backward pass (torch)
-    # y_ref.backward(dy, retain_graph=True)
-    # dx_ref, dw_ref, db_ref = [_.grad.clone() for _ in [x, weight, bias]]
+    z_out_tri.backward(dy, retain_graph=True)
+    # dw_tri, db_tri
+    dz_tri, dx_tri  = [_.grad.clone() for _ in [z, x]] #, weight, bias]]
+    # print(dz_tri.size())
+    print(dz_tri[0], dz_tri[1], dz_tri[-1])
+    x.grad = None #, weight.grad, bias.grad = None, None, None
+    # backward pass (torch)
+    z_out_ref.backward(dy, retain_graph=True)
+    dz_ref, dx_ref = [_.grad.clone() for _ in [z, x]] #, weight, bias]]
     # # # compare
+    print(dz_tri[0], dz_tri[1], dz_tri[-1])
+    
     # # assert torch.allclose(y_tri, y_ref, atol=1e-2, rtol=0)
     # assert torch.allclose(dx_tri, dx_ref, atol=1e-2, rtol=0)
     # assert torch.allclose(db_tri, db_ref, atol=1e-2, rtol=0)
